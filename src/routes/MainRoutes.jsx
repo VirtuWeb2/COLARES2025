f63ed87c25f11fcb8753f776411dd85f0a1ffba2
tcordeirojr <tedcordeirojr@gmail.com>
2025-11-07 14:52:24 -0300

    up

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -23,7 +23,7 @@ function App() {
     async () => {
       return (
         await axios.get("https://api-sites-en.vercel.app/news")
-      ).data.sort((a, b) => b.id - a.id).filter((n)=> n.muni === "altamira" || n.muni === "pará");
+      ).data.sort((a, b) => b.id - a.id).filter((n)=> n.muni === "colares" || n.muni === "pará");
     },
   );
 
@@ -32,7 +32,7 @@ function App() {
     async () => {
       return (await axios.get("https://api-sites-en.vercel.app/tv")).data.sort(
         (a, b) => b.id - a.id
-      ).filter(t=> t.muni === "altamira" || t.muni === "pará");
+      ).filter(t=> t.muni === "colares" || t.muni === "pará");
     }
   );
   return (
